116c6e3766c966b5215a7afe98b68ec65b4c353e
Konstantinos <kwstas654321@gmail.com>
2021-02-20 01:28:57 +0200

    Delete SQL_Joins.sql

--- MySQL_Excercises/SQL_Joins.sql
+++
@@ -1,9 +0,0 @@
-USE `classicmodels`;
-SELECT 
-    `products`.`productCode`,
-    `products`.`productName`,
-    `productlines`.`textDescription`
-FROM
-    `products`
-        INNER JOIN
-    `productlines` ON `products`.`productLine` = `productlines`.`productLine`;
