c4d6e89e283de089c4c3c74b23f4de7d6099dafc
Konstantinos <kwstas654321@gmail.com>
2021-02-20 01:29:14 +0200

    Create Inner.sql

---
+++ MySQL_Excercises/SQL_JOINS/Inner.sql
@@ -0,0 +1,14 @@
+-- INNER JOIN  
+USE `classicmodels`;
+SELECT 
+    `products`.`productCode`,
+    `products`.`productName`,
+    `productlines`.`textDescription`
+FROM
+    `products`
+        INNER JOIN
+    `productlines` ON `products`.`productLine` = `productlines`.`productLine`;
+    
+
+
+	
