51e3292d900bd697ed934e3a97e08b7e74034057
Konstantinos <kwstas654321@gmail.com>
2021-02-20 13:00:49 +0200

    Update delete.sql

--- MySQL_Excercises/Triggers/delete.sql
+++ MySQL_Excercises/Triggers/delete.sql
@@ -6,3 +6,19 @@ CREATE TABLE `billings` (
     `amount` DEC(10 , 2 ),
     PRIMARY KEY (billingNo)
 );
+
+USE `classicmodels`;
+
+DELIMITER $$
+CREATE TRIGGER `before_billing_update`
+	BEFORE UPDATE 
+    ON `billings` FOR EACH ROW
+BEGIN
+	IF new.`amount` > old.`amount` * 10 THEN 
+		SIGNAL SQLSTATE '45000'
+			SET MESSAGE_TEXT = 'New amount cannot be 10 times greater than the current amount.';
+		END IF;
+END$$
+DELIMITER ;
+
+
