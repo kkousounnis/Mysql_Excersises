07b0a4b0d6062cba07c0927f89d9e95934b7fe42
Konstantinos <kwstas654321@gmail.com>
2021-02-20 12:34:06 +0200

    Create delete.sql

---
+++ MySQL_Excercises/Triggers/delete.sql
@@ -0,0 +1,8 @@
+USE `classicmodels`;
+CREATE TABLE `billings` (
+    `billingNo` INT AUTO_INCREMENT,
+    `customerNo` INT,
+    `billingDate` DATE,
+    `amount` DEC(10 , 2 ),
+    PRIMARY KEY (billingNo)
+);
